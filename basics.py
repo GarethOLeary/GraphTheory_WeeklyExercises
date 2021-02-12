f1a2dd4b87bc92bd0fca36bc22c2f27b03040caf
Gareth O'Leary <G00375321@gmit.ie>
2021-02-12 16:58:11 +0000

    Looked at the basics

---
+++ basics.py
@@ -0,0 +1,13 @@
+#Gareth O'Leary
+#Python Basics
+
+print("Hello World")
+
+a =1
+b = 1.0
+s = "Hello, world from a string!"
+t = 'Hello, from a different string'
+
+print (a,b,s,t)
+
+print(s[3:10:2])
