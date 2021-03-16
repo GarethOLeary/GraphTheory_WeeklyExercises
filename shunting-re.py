701194022634ce76e02b0648c2cc597734a5838d
Gareth O'Leary <G00375321@gmit.ie>
2021-03-16 09:18:53 +0000

    Shunting Yard Algorithm for Regular Expressions

---
+++ shunting-re.py
@@ -0,0 +1,56 @@
+# Adapted from the pseudocode at:
+# https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+
+
+def shunt(infix):
+    """Convert infix expressions to postfix."""
+    # The eventual output.
+    postfix = ""
+    # The shutting yard operator stack.
+    stack = ""
+    # Operator precedence
+    prec = {'*': 100, '.': 90, '|': 80}
+    # Loop through the input of a character at a time 
+    for c in infix:
+        # c is a digit 
+        if c in {'a', 'b'}:
+            # Push it to the output
+            postfix = postfix + c
+        # c is an operator 
+        elif c in {'*', '.', '|'}:
+            # Check what is on the stack
+            while len(stack) > 0 and stack[-1] != '(' and prec[stack[-1]] >= prec[c]:
+                # Append operator at the top of stack to output
+                postfix = postfix + stack[-1]
+                # Remove operator from stack
+                stack = stack[:-1]
+            # Push c to stack
+            stack = stack + c
+        elif c == '(':
+             # Push c to stack
+             stack = stack + c
+        elif c == ')':
+            while stack[-1] != "(":
+                # Append operator at the top of stack to output
+                postfix = postfix + stack[-1]
+                # Remove operator from stack
+                stack = stack[:-1]
+            # Remove open bracket from stack
+            stack = stack[:-1]
+    #Empty the operator stack
+    while len(stack) != 0:
+        # Append operator at the top of stack to output
+        postfix = postfix + stack[-1]
+        # Remove operator from stack
+        stack = stack[:-1]
+    return postfix
+
+if __name__ == "__main__":
+    for infix in ["a.(b.b)*.a"]:
+        print(f"infix: {infix}")
+        print(f"postfix: {shunt(infix)}")
+        print()
+
+
+
+
